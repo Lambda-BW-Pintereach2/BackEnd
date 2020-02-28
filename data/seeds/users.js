d43a622761248bc9dce51169628aa2ccba8d794a
Shafi Masoumi <masoumi2017@icould.com>
2020-02-27 18:07:00 -0800

    restricted middleware and models added

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
