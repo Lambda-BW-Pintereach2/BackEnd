f44e123f7a87bd0f74742356b3909545fe9f94e9
Shafi Masoumi <masoumi2017@icould.com>
2020-02-27 16:50:31 -0800

    seed file and migrations done

---
+++ data/seeds/users.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          email: 'masoumi2017@icloud.com',
+          username: 'shafi',
+          password: bcrypt.hashSync('pass123', 10)
+        },
+
+        { email: 'jay@yahoo.com',
+          username: 'jay',
+          password: bcrypt.hashSync('pass345', 10)
+        },
+      ]);
+    });
+};
