dffb9cb50fb480a4a967b2a69a38398ee955fcf7
Shafi Masoumi <masoumi2017@icould.com>
2020-03-04 11:47:45 -0800

    updates on seed files

--- data/seeds/articles.js
+++ data/seeds/articles.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("articles").truncate()
+  return knex("articles").del()
     .then(function () {
       // Inserts seed entries
       return knex("articles").insert([
