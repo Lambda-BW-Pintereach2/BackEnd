93a839f0fc2d876fe15cc13238ac010296da2a99
Shafi Masoumi <masoumi2017@icould.com>
2020-03-04 11:42:54 -0800

    tables updated

--- data/migrations/20200227160757_users.js
+++ data/migrations/20200227160757_users.js
@@ -1,13 +1,30 @@
 
-exports.up = function(knex) {
+exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', tbl => {
-      tbl.increments();
-      tbl.string('email').unique();
-      tbl.string('username', 128).notNullable().unique();
-      tbl.string('password', 128).notNullable();
+    tbl.increments();
+    tbl.string('email').unique();
+    tbl.string('username', 128).notNullable().unique();
+    tbl.string('password', 128).notNullable();
   })
+
+    .createTable("articles", tbl => {
+      tbl.increments();
+      tbl.string("title", 250).notNullable();
+      tbl.string("description", 1000).notNullable();
+      tbl.string("image", 250);
+      tbl.string("link");
+      tbl.string("category", 250);
+      tbl.integer("user_id").unsigned().references("id").inTable("users").onDelete("CASCADE").onUpdate("CASCADE")
+    })
+    .createTable("category", tbl => {
+      tbl.increments();
+      tbl.string("name", 255)
+      tbl.integer("articles_id").unsigned().references("id").inTable("articles").onDelete("CASCADE").onUpdate("CASCADE")
+    })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTableIfExists('users')
+    .dropTableIfExists("articles")
+    .dropTableIfExists("category")
 };
--- data/seeds/category.js
+++ data/seeds/category.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("category").del()
+  return knex("category").truncate()
     .then(function () {
       // Inserts seed entries
       return knex("category").insert([
