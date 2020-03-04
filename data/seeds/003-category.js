48e56e282101970bf98191a1489c8cd5af89c2b8
Shafi Masoumi <masoumi2017@icould.com>
2020-03-04 12:48:03 -0800

    changes on seed file

--- data/seeds/003-category.js
+++ data/seeds/003-category.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex("category").insert([
-        {name: "Games", articles_id: 1},
-        {name: "Fashion", articles_id: 2},
-        {name: "Other", articles_id: 3}
+        {name: "Games", article_id: 1},
+        {name: "Fashion", article_id: 2},
+        {name: "Other", article_id: 3}
       ]);
     });
 };
